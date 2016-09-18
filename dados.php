f037516cdcd70c847c42c9622d0d00c4c3bc0d34
IcaroCleto <icarocleto.sousa@outlook.com>
2016-09-18 02:29:32 -0300

    Função de rastrear IP

--- dados.php
+++ dados.php
@@ -1,14 +1,15 @@
 <?php
 // Dados do Servidor
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "192.95.43.212";
+$username = "gama";
+$password = "t!m305";
 $dbname = "descomplica";
 
 // Dados do Usuario
 $nome = $_GET['nome'];
 $email = $_GET['email'];
 $data = date('d/m/Y H:i:s');
+$ip = $_SERVER['SERVER_ADDR'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,8 +18,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pessoas (nome, email, data_cad)
-VALUES ('".$nome."','".$email."','".$data."')";
+$sql = "INSERT INTO pessoas (nome, email, data_cad, ip)
+VALUES ('".$nome."','".$email."','".$data."','".$ip."')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
