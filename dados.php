e14cfba44c2f5a344107967d85d27b3f9adb3774
IcaroCleto <icarocleto.sousa@outlook.com>
2016-09-18 22:47:09 -0300

    Version 2.0

--- dados.php
+++ dados.php
@@ -1,12 +1,20 @@
 <?php
-// Dados do Servidor
+// Dados do Servidor Local
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "descomplica";
+
+// Dados do Servidor Web
 $servername = "192.95.43.212:3306";
 $username = "gama";
 $password = "t!m305";
 $dbname = "descomplica";
+
 // Dados do Usuario
 $nome = $_GET['nome'];
 $email = $_GET['email'];
+
 
 //Get IP connection
 $ip = getenv('HTTP_CLIENT_IP')?:
@@ -27,11 +35,11 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO pessoas (nome, email, data_cad, ip)
 VALUES ('".$nome."','".$email."',NOW(),'".$ip."')";
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    header("Location: livro.php");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-header("Location: index.html");
+
 $conn->close();
 
 
---
+++ livro.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Vaganafacul - Livro</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="all,follow">
+  <!-- Bootstrap and Font Awesome css-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+  <!-- Google fonts-->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Pacifico">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,700">
+  <!-- Theme stylesheet-->
+  <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+  <!-- Custom stylesheet - for your changes-->
+  <link rel="stylesheet" href="css/stylemodal.css">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="img/faviconicon.jpg">
+  <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
+<body>
+  <div class="modal-dialog text-center fundo">
+      <!-- Modal content-->
+      <div class="modal-content quadro-download">
+        <div class="modal-header">
+          <a href="index.html"><button type="button" class="close" data-dismiss="modal">&times;</button></a>
+          <h4 class="modal-title">Essa #vaganafacul é sua!</h4>
+        </div>
+        <div class="modal-body">
+          <p>Você recebera um email com dicas matadoras ainda essa semana !</p>
+          <p>Obrigado por participar!</p>
+          <a href="index.html"><button type="button" class="btn btn-success" data-dismiss="modal">Voltar</button></a>
+
+        </div>
+      </div>
+
+    </div>
+
+</body>
+</html>
