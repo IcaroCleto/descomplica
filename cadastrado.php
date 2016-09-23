358d5495d210dc220735927c0bad29cb4ae3889a
IcaroCleto <icarocleto.sousa@outlook.com>
2016-09-23 01:47:52 -0300

    PHP 100% Funcional

---
+++ cadastrado.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Vaganafacul - Livro</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="all,follow">
+  <!-- Bootstrap and Font Awesome css-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+  <!-- Google fonts-->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Pacifico">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,700">
+  <!-- Theme stylesheet-->
+  <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+  <!-- Custom stylesheet - for your changes-->
+  <link rel="stylesheet" href="css/stylemodal.css">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="img/faviconicon.jpg">
+  <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   <!-- Facebook Pixel Code -->
+   <script>
+         !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+         n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+         n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+         document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '618201588272472');
+         fbq('track', "PageView");</script>
+          <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=618201588272472&ev=PageView&noscript=1"
+     /></noscript>
+        <!-- End Facebook Pixel Code -->
+</head>
+<body>
+  <div class="modal-dialog text-center fundo">
+      <!-- Modal content-->
+      <div class="modal-content quadro-download">
+        <div class="modal-header">
+          <a href="index.html"><button type="button" class="close" data-dismiss="modal">&times;</button></a>
+          <h4 class="modal-title">Essa #vaganafacul Ã© sua!</h4>
+        </div>
+        <div class="modal-body">
+          <p>Este email já está cadastrado !</p>
+          <p>Clique em voltar e se cadastre com um email diferente, obrigado!</p>
+          <a href="index.html"><button type="button" class="btn btn-success" data-dismiss="modal">Voltar</button></a>
+
+        </div>
+      </div>
+
+    </div>
+
+</body>
+</html>
--- dados.php
+++ dados.php
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-<?php
-// Dados do Servidor
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "descomplica";
-
-// Dados do Usuario
-$nome = $_GET['nome'];
-$email = $_GET['email'];
-$data = date('d/m/Y H:i:s');
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO pessoas (nome, email, data_cad)
-VALUES ('".$nome."','".$email."','".$data."')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-header("Location: index.html");
-$conn->close();
-=======
 <?php
 // Dados do Servidor Local
 // $servername = "localhost";
@@ -44,10 +12,8 @@ $password = "t!m305";
 $dbname = "descomplica";
 
 // Dados do Usuario
-$nome = $_GET['nome'];
-$email = $_GET['email'];
-
-
+$nome = isset($_GET['nome']) ? $_GET['nome'] : NULL;
+$email = isset($_GET['email']) ? $_GET['email'] : NULL;
 //Get IP connection
 $ip = getenv('HTTP_CLIENT_IP')?:
 getenv('HTTP_X_FORWARDED_FOR')?:
@@ -55,11 +21,17 @@ getenv('HTTP_X_FORWARDED')?:
 getenv('HTTP_FORWARDED_FOR')?:
 getenv('HTTP_FORWARDED')?:
 getenv('REMOTE_ADDR');
-
 	//End IP Connection
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$sql = $conn->query("SELECT * FROM pessoas WHERE email='".$email."'");
+if(mysqli_num_rows($sql) > 0){
+    echo 'Email já cadastrado no Banco de Dados!';
+    header("Location: cadastrado.php");
+    exit();
+}
+else {
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -71,13 +43,6 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 $conn->close();
-<<<<<<< HEAD
->>>>>>> cb5aa6aa6314410bcf52c2630ddd599361360f38
-=======
-
-
-
->>>>>>> f9d91c725b0d8094ba69f330c039ed722739e047
+}
 ?>
